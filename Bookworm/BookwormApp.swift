3b431919a515d7dee53d5d776da6f04be49587a2
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-07-02 10:07:55 -0400

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Gabriel Marquez on 2022-07-02.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
